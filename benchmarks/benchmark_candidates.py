dfbc6ebc1cee41d9fb844af446fbd8d1f7f943ed
ivanmkc <ivanmkc@google.com>
2025-12-26 11:51:44 +0900

    Enable full benchmark suite

--- benchmarks/benchmark_candidates.py
+++ benchmarks/benchmark_candidates.py
@@ -33,20 +33,19 @@ _podman_image_dirs = [
     # Path("benchmarks/answer_generators/gemini_cli_docker/mcp_adk_agent_runner"),
 ]
 
-# CANDIDATE_GENERATORS = list(permute(
-#     GeminiCliPodmanAnswerGenerator,
-#     model_name=[ModelName.GEMINI_2_5_FLASH], #, ModelName.GEMINI_2_5_PRO],
-#     dockerfile_dir=_podman_image_dirs,
-#     image_definitions=[IMAGE_DEFINITIONS],
-#     api_key_manager=[api_key_manager]
-# ))
-CANDIDATE_GENERATORS = []
+CANDIDATE_GENERATORS = list(permute(
+    GeminiCliPodmanAnswerGenerator,
+    model_name=[ModelName.GEMINI_2_5_FLASH], #, ModelName.GEMINI_2_5_PRO],
+    dockerfile_dir=_podman_image_dirs,
+    image_definitions=[IMAGE_DEFINITIONS],
+    api_key_manager=[api_key_manager]
+))
 
 CANDIDATE_GENERATORS.extend([
-    # create_workflow_adk_generator(
-    #     model_name=ModelName.GEMINI_2_5_FLASH,
-    #     api_key_manager=api_key_manager
-    # ),
+    create_workflow_adk_generator(
+        model_name=ModelName.GEMINI_2_5_FLASH,
+        api_key_manager=api_key_manager
+    ),
     create_structured_workflow_adk_generator(
         model_name=ModelName.GEMINI_2_5_FLASH,
         api_key_manager=api_key_manager
